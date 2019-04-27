e140a1a5ee7e1ea85229451bb90fa1b61b1911e5
Markreel <markmeerssman@live.nl>
2019-04-27 17:26:28 +0200

    Adjusted camera setup

---
+++ LudumDare44-YourLifeIsCurrency/Assets/Scripts/GodManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GodManager : MonoBehaviour
+{
+    public enum Choices { WarGod, OceanGod, VolcanoGod, FurtilityGod}
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void OnChoiceMade(Choices _choice)
+    {
+
+    }
+}
---
+++ LudumDare44-YourLifeIsCurrency/Assets/Scripts/UIManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+
+{
+    public bool IsActive = true;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void ChangeActivity(bool _isActive)
+    {
+        GetComponent<GraphicRaycaster>().enabled = IsActive = _isActive;
+    }
+}

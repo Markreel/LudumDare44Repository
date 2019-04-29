e39cc3d8efc2849d30fbf798edd755e0b8065f65
Markreel <markmeerssman@live.nl>
2019-04-29 17:29:00 +0200

    Added more cutscenes Started working on wind animations

---
+++ LudumDare44-YourLifeIsCurrency/Assets/Scripts/NPCSpawnActivator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCSpawnActivator : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        AIManager.Instance.SpawnNPCs(15, 3);
+    }
+
+}
